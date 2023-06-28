90d0f60cb72fb89310ba4928e14ed3ff484f5130
Simon <simon@aam-digital.com>
2023-06-28 10:45:18 +0200

    refactor: removed unused files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CouchdbAdminController } from './couchdb-admin/couchdb-admin.controller';
 import { HttpModule } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [HttpModule, ConfigModule.forRoot({ isGlobal: true })],
-  controllers: [AppController, CouchdbAdminController],
-  providers: [AppService],
+  controllers: [CouchdbAdminController],
 })
 export class AppModule {}
