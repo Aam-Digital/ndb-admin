062100311d7ba60e024869cb674ad92832349abd
Simon <simon@aam-digital.com>
2023-10-26 11:47:21 +0200

    added folder for keycloak services

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,17 @@ import { CouchdbAdminController } from './couchdb/couchdb-admin.controller';
 import { HttpModule } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
 import { CouchdbService } from './couchdb/couchdb.service';
-import { KeycloakService } from './couchdb/keycloak.service';
+import { KeycloakService } from './keycloak/keycloak.service';
 import { MigrationController } from './couchdb/migration.controller';
+import { KeycloakMigrationController } from './keycloak/keycloak-migration.controller';
 
 @Module({
   imports: [HttpModule, ConfigModule.forRoot({ isGlobal: true })],
-  controllers: [CouchdbAdminController, MigrationController],
+  controllers: [
+    CouchdbAdminController,
+    MigrationController,
+    KeycloakMigrationController,
+  ],
   providers: [CouchdbService, KeycloakService],
 })
 export class AppModule {}
