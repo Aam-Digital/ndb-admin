b55bea792e6c5a01f8bfe3d2b0d15247dd8dea47
Simon <simon@aam-digital.com>
2023-11-01 17:58:16 +0100

    fetching existing config to check whats missing

--- src/keycloak/keycloak-migration.controller.ts
+++ src/keycloak/keycloak-migration.controller.ts
@@ -22,72 +22,68 @@ export class KeycloakMigrationController {
   }
 
   private async updateRealm(realm: string, realmConfig: RealmConfig) {
+    const currentConfig = await this.createResource<RealmConfig>(
+      `${realm}/partial-export`,
+    );
     await this.updateResource(realm, realmConfig);
     await this.alignResources(
       realmConfig.clientScopes,
+      currentConfig.clientScopes,
       'name',
       `${realm}/client-scopes`,
     );
     await this.alignResources(
       realmConfig.authenticationFlows,
+      currentConfig.authenticationFlows,
       'alias',
       `${realm}/authentication/flows`,
     );
-    // await this.alignResources(
-    //   realmConfig.authenticatorConfig,
-    //   'alias',
-    //   `${realm}/authentication/flows`,
-    // );
+    await this.alignResources(
+      realmConfig.authenticatorConfig,
+      currentConfig.authenticatorConfig,
+      'alias',
+      `${realm}/authentication/flows`,
+    );
   }
 
   /**
    * Aligns realm resource from config with actual on server
    * TODO deleting and updating is not supported yet. Only creating new.
-   * @param resources
-   * @param property
-   * @param path
    * @private
    */
   private async alignResources<T, P extends keyof T>(
-    resources: T[],
+    update: T[],
+    existing: T[],
     property: P,
     path: string,
   ) {
-    const existingResources = await this.getResource<T[]>(`realms/${path}`);
-    console.log(
-      'existing',
-      path,
-      existingResources.map((r) => r[property]),
+    const missingResources = update.filter(
+      (r) => !existing.some((c) => c[property] === r[property]),
     );
-    console.log(
-      'resources',
-      path,
-      resources.map((r) => r[property]),
-    );
-    const missingResources = resources.filter(
-      (r) => !existingResources.some((c) => c[property] === r[property]),
-    );
-    console.log('missing', path, missingResources);
-    // return missingResources.map((scope) => this.createResource(path, scope));
+    return missingResources.map((scope) => this.createResource(path, scope));
   }
 
-  private createResource(path: string, resource) {
+  private createResource<T = any>(path: string, resource?) {
     return firstValueFrom(
-      this.http.post(
-        `${this.keycloak.keycloakUrl}/admin/realms/${path}`,
-        resource,
-        this.config,
-      ),
+      this.http
+        .post<T>(
+          `${this.keycloak.keycloakUrl}/admin/realms/${path}`,
+          resource,
+          this.config,
+        )
+        .pipe(map((res) => res.data)),
     );
   }
 
-  private async updateResource(path: string, resource: any) {
-    await firstValueFrom(
-      this.http.put(
-        `${this.keycloak.keycloakUrl}/admin/realms/${path}`,
-        resource,
-        this.config,
-      ),
+  private updateResource<T = any>(path: string, resource?: any) {
+    return firstValueFrom(
+      this.http
+        .put<T>(
+          `${this.keycloak.keycloakUrl}/admin/realms/${path}`,
+          resource,
+          this.config,
+        )
+        .pipe(map((res) => res.data)),
     );
   }
 
