b613c6fa66b59f0593eb03b0ef3bf3bc9a353a05
Sebastian <sebastian@aam-digital.com>
2025-04-07 08:53:58 +0200

    fix: add Sentry logging to ndb-admin (#52)

---
+++ src/sentry-configuration.ts
@@ -0,0 +1,79 @@
+import * as Sentry from '@sentry/node';
+import { ConfigService } from '@nestjs/config';
+import { ArgumentsHost, INestApplication } from '@nestjs/common';
+import { BaseExceptionFilter, HttpAdapterHost } from '@nestjs/core';
+import { HttpException } from '@nestjs/common/exceptions/http.exception';
+
+export class SentryConfiguration {
+  ENABLED: boolean = false;
+  DSN = '';
+  INSTANCE_NAME = '';
+  ENVIRONMENT = '';
+}
+
+function loadSentryConfiguration(
+  configService: ConfigService,
+): SentryConfiguration {
+  return {
+    ENABLED: configService.get('SENTRY_ENABLED', false),
+    DSN: configService.get('SENTRY_DSN', ''),
+    INSTANCE_NAME: configService.get('SENTRY_INSTANCE_NAME', 'Admin API'),
+    ENVIRONMENT: configService.get('SENTRY_ENVIRONMENT', 'development'),
+  };
+}
+
+export function configureSentry(
+  app: INestApplication,
+  configService: ConfigService,
+): void {
+  const sentryConfiguration = loadSentryConfiguration(configService);
+  if (sentryConfiguration.ENABLED) {
+    configureLoggingSentry(app, sentryConfiguration);
+  }
+}
+
+export class SentryFilter extends BaseExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    Sentry.captureException(exception);
+    super.catch(exception, host);
+  }
+}
+
+function configureLoggingSentry(
+  app: INestApplication,
+  sentryConfiguration: SentryConfiguration,
+): void {
+  Sentry.init({
+    debug: true,
+    serverName: sentryConfiguration.INSTANCE_NAME,
+    environment: sentryConfiguration.ENVIRONMENT,
+    dsn: sentryConfiguration.DSN,
+    integrations: [
+      // enable HTTP calls tracing
+      Sentry.captureConsoleIntegration(),
+      Sentry.httpIntegration(),
+    ],
+    // Performance Monitoring
+    tracesSampleRate: 1.0, //  Capture 100% of the transactions
+    // Set sampling rate for profiling - this is relative to tracesSampleRate
+    profilesSampleRate: 1.0,
+
+    beforeSend: (event, hint) => {
+      const error = hint.originalException;
+      if (
+        error instanceof HttpException &&
+        error.getStatus() >= 400 &&
+        error.getStatus() < 500
+      ) {
+        return null;
+      }
+
+      return event;
+    },
+  });
+
+  app.use(Sentry.expressErrorHandler());
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new SentryFilter(httpAdapter));
+}
